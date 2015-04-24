31aae446a929c1bda9b8c13461d676058f6926f0
josomo <jsmokry@gmail.com>
2015-04-24 17:04:45 -0400

    Create app.rb and create RESTful route structure

---
+++ app.rb
@@ -0,0 +1,22 @@
+require 'sinatra'
+
+get '/' do
+end
+
+get '/user' do
+end
+
+post '/user' do
+end
+
+put '/user' do
+end
+
+delete '/user' do
+end
+
+post '/auth' do
+end
+
+delete '/auth' do
+end
